b7978c4f03ee56b4a4c4758c2aaae55751eb3261
Michael Plaksyuk <mplaksyuk@fire>
2021-11-24 22:19:55 +0200

    add device information block, create jquery functions

--- site/dev/js/script.js
+++ site/dev/js/script.js
@@ -5,4 +5,22 @@ $(function(){
         .closest('.device-item-bottom').find('.thermometers').toggleClass('d-none').end()
         .closest('.device-item-bottom').find('.toggle-device').toggleClass('choose').end();
     })
+
+    $('.device').on('click', function(){
+        $('.all-device-information-content').empty();
+        const cln = $(this).clone();
+        cln.appendTo('.all-device-information-content');
+        
+        const element = cln.children().attr('class');
+        cln.find("."+`${element}`+"-information").removeClass('d-none');
+
+
+    })
+
+    $('.all-device-information-content').on('dblclick', function(){
+        $(this).empty();
+    })
+
+
+
 })
